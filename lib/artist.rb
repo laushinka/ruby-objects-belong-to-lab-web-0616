55502ea7f40fb8a8a456c2c474660b6b2a6e0f04
laushinka <laurie.malau@gmail.com>
2016-06-11 19:53:21 -0400

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,7 @@
+class Artist
+  attr_accessor :name
+
+end
+#
+# lala = Artist.new("Beyonce")
+# puts lala.name
---
+++ lib/post.rb
@@ -0,0 +1,8 @@
+class Post
+  attr_accessor :title, :author, :post
+
+  def post(post)
+    post.author = self
+  end
+  
+end
